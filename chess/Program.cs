7c24a27b86ec93f86606d6e721df75988e77c80c
Makar85 <Levitin85@yandex.ru>
2021-11-24 15:27:50 +0300

    AddChessProject

---
+++ chess/Program.cs
@@ -0,0 +1,42 @@
+﻿using System;
+
+/*
+ Напишите программу, которая определяет корректность хода ферзя, заданного в шахматной нотации, 
+ например “e2-f4” – некорректный, а “c2-c3”, “c2-e2”, “c2-f5” – корректные ходы.
+ */
+
+namespace chess
+{
+    class Program
+    {
+        static bool Queenmove(string notation)
+        {
+            string[] cells = notation.Split("-");
+            char[] srcCell = cells[0].ToCharArray();
+            char[] dstCell = cells[1].ToCharArray();
+
+            int deltaX = Math.Abs(srcCell[0] - dstCell[0]);
+            int deltaY = Math.Abs(srcCell[1] - dstCell[1]);
+
+            if (deltaX == 0 && deltaY == 0)
+                return false;
+            if (deltaX == deltaY)
+                return true;
+            if (deltaX == 0 ^ deltaY == 0)
+                return true;
+            else
+                return false;
+
+        }
+        static void Main(string[] args)
+        {
+            Console.Write("Enter step: ");
+            string step = Console.ReadLine();
+            bool result = CheckStep(step);
+            if (result == true)
+                Console.WriteLine("Allowed");
+            else
+                Console.WriteLine("Denied");
+        }
+    }
+}
